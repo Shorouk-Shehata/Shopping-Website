c64b30592716a2680e45f6d1bed19eaf6c32681a
Shorouk-Shehata <104050013+Shorouk-Shehata@users.noreply.github.com>
2023-07-08 00:01:24 +0300

    Add files via upload

---
+++ firest-project/css-style/css/script.js
@@ -0,0 +1,169 @@
+function test6(){
+    alert('Product details:-\n\nFully-fashioned trainers with a ribbed top edge and loop at the back. Mesh insoles, and contrasting colour, patterned soles.\nthe price: EGP 649.00')
+}
+function test7(){
+    alert('*Portofino leather sandals*\n\nProduct details:-\n SKU :G6095385RICNAP-NERO\n Made: in Italy\nColour: Black\n Designer: Gianvito Rossi\n Heel Size: 8.5cm\nMaterial: Leather\n Size and Fit Fits: true to size, take your normal size\nthe price: 2,810 AEDice: ')
+
+}
+function test8(){
+    alert('*Maureen pumps*\n\nProduct details:-\nSKU MAUP7028-GLD\nMade in Italy\nColour Gold\n Designer Malone Souliers\n Heel Size 7cm\n Material Leather\n Size and Fit Fits true to size, take your normal size\nthe price: 2,320 AED')
+
+}
+function test1(){
+    alert('*Black leather blucher shoes*\n\nProduct details:-\nOxford fabric design. 100% cowhide leather. laces; Round toe shoe. Leather lining and sole. rubber sole \nthe price:EGP 2,299')
+}
+function test12 (){
+    alert('*White leather shoes*\n\nProduct details:-\nSole Material: Rubber\n Style: Casual Toe \nStyle:Pointed toe\n Heel High Style:Slip-on Platform\n Height: 3cm \nPackage:1 Pair men Shoes(Not Including Shoebox)\nthe price:EGP 448 ')
+}
+function test15(){
+    alert('*Starter Leather Reflect Kids Sneaker - White*\n\nProduct details:-\nUpper :  Synthitec Leather.Insole : Cotton jersey .\nMidsole : Single-density moulded PU.\nSole : Anti-abrasion PU.\nthe price: EGP 280')
+}
+function test14(){
+    alert('*Toobaco Casual Leather Shoes For Girls*\n\nProduct details:-\nGirls casual shoes\nMade from the best and finest imported materials\n Wonderful and unique design\nSuitable for all tastes and ages\nVery comfortable for feet with a modern design\nColor: Mint Green\n\nMain Material: Leather\nthe price:EGP 125 ')
+}
+function test13(){
+    alert('*Toobaco Half Boot Girls Casual Cloth*\n\nProduct details:-\nFactory from the best and finest imported cloth materials\nNew design and more wonderful\nColor: black\n Main Material: cloth\nIt suits all ages and tastes\nComfortable sole to ensure the best foot comfort experience\nthe price: EGP 145')
+}
+function test4(){
+    alert('*Fashionable sneakers*\n\nProduct details:-\nAir Walk Huge Rubber Sole Fashionable Sneakers - Yellow, Black & Taupe\nLeather & Mesh Upper Material\nTextile inner material\nRubber Sole\nRound toecap shape\nLace Up Closure\nColor: Multicolour\n Main Material: Mix\nthe price:EGP 249')
+}
+function test3(){
+    alert('*General Shoes CLASSIC Mid Heels*\n\nProduct details:-\nShoes CLASSIC Mid HeelsShoes CLASSIC Mid HeelsShoes CLASSIC Mid HeelsInner lining color may vary\nColor: Black\nMain Material: lather\n Model: classic B 37\nthe price:EGP 159')
+}
+function test9(){
+        alert('*General Shoes CLASSIC Mid Heels*\n\nProduct details:-\nHigh quality with the finest materials\nHigh reliability\nEase of use for life\nColor: Black\nMain Material: Suede\nModel: z1\nthe price:EGP 125 - EGP 149 ')
+}
+function test5(){
+    alert('*General Women Boot Heels Boots Above Ankle*\n\nProduct details:-\nWoman Boot Heels Boots Above Ankle-Mixed\nHieght:10cm\nColor: BlackMain Material: Mix\nModel: 11110\nthe Price :EGP 239')
+}
+function test10(){
+    alert('*Women Casual Flat Shoes*\n\nProduct details:-\nFlat Casual Shoes for women with finest quality\ncolor :- Silver\navailable sizes starting from 37 to 41\nSupreme quality \nOne of the finest Egyptian manufacture.\nthe price :EGP 245')
+}
+function test11(){
+    alert('*Fashion Children Comfortable Sandals Outdoor Shoes*\n\nProduct details:-\nExcellent workmanship ,Wear-resisting,Anti-skidding ,Comfortable ,Breathable, Durable, Soft Nice material,Comfortable,Flat bottom,and Easy for walk .\nColor: Purple\nMain Material: Other\nthe price :EGP 300')
+}
+function test2(){
+    alert('*RED WING SHOES*\n\nProduct details:-\n Leather Type: Full Grain, Water-Resistant\nUpper: Brown Chrome Leather\nConstruction: SuperSole Welt\nInsole: PORON\nShank: Fiberglass\nModel: 8249\nOutsole: Mini Lug SuperSole - Black\nCare Products: Boot Oil\nthe price :EGP 3,300 - EGP 3,500')
+}
+function text1(){
+    alert('*Hoover full automatic washing machine*\n\nProduct details:-\nWashing machine capacity: 8 kg\nRotation rate: 1300 rpm\nsmart washing system\nALL IN ONE TECHNOLOGY\nThe inner pot is made of stainless steel\nCountry of Origin: Italy\n5 year full free warranty ')
+}
+function text2(){
+    alert('*Samsung Washing Machine*\n\nProduct details:-\nCapilty is 18 kg  top loading\nstainless steel color\ndigital control\nWobble technology\ndimensions 630 x 1,100 x 690 mm\nthe price :EGP 12,948 ')
+}
+function text3(){
+    alert('*TWINWash™*\n\nProduct details:-\nseparate wash\nSave time Cut your washing time in half, you can now finish 2 loads of laundry in 49 minutes with LG TWINWash™\nReduce allergens by 99.9%\nInverter Direct Drive Motor, Less Vibration, Less Noise')
+}
+function text4(){
+    alert('*Candy Washing Machine*\n\nProduct details:-\nCandy Washing Machine Full Automatic 7 Kg\n Rotation rate: 1000 rpm\nWashing machine body color: white\nLCD digital display\nCountry of Origin: Italy\n5 years full free warranty\nthe price :7,099 EGP')
+}
+function text5(){
+    alert('*tornado screen*\n\nProduct details:-\nScreen size: 58 inch\nScreen design: flat\nScreen Resolution: 4K Ultra HD\nScreen type: LED\nIndoor receiver\nSmart Connectivity: WiFi\nConnectivity: 2 USB, 3 HDMI\nthe price : EGP 7,749')
+}
+function text6(){
+    alert('*Samsung TV*\n\nProduct details:-\nScreen size 32 inch\nHDMI 2 No. of inputs\n(USB) 1 Number of USB ports\nSmart No\nHeadphone Out Yes\nLED screen type\n1364 x 720 screen resolution\nBuilt-in Receiver Yes \nthe price :3,899 EGP')
+}
+function text7(){
+    alert('*Toshiba TV*\n\nProduct details:-\nToshiba 55 inch 4K Smart LED TV Without Frame Ultra HD\nEquipped with an internal receiver\nThe ability to connect to the Internet, wired and wireless\nAccess to the Netflix app to watch the latest series and movies\nCountry of Origin: Egypt\n2 Years Full Free Warranty\nthe price :8,999 EGP ')
+}
+function text8(){
+    alert('*Fresh Gas Cooker*\n\nProduct details:-\nBrand: Fresh\nWeight: 50 kg\nDimensions: 60 x 90 cm\nblack color\nFuel type: gas\nNumber of burners: 5\nThere is a double knob to operate the oven and grill.\nThe oven door is made entirely of glass\nThe gas stove has a self-ignition feature.\nThe oven has a fan\nThe oven also has a light bulb\nMaterial: stainless steel surface\nthe price :3,999.00 EGP ')
+}
+function text9(){
+    alert('*Premium Double Chef Gas Cooker*\n\nProduct details:-\nburners cooker  5\nStainless surface\nFull safety\nTimer Touch\nControl gas separation\nOven fan\nSelf-ignition of keys and timer\nOven hydraulics\nGrill motor  2\nhorizontal oven  2\nthe price :EGP 8,999')
+}
+function text10(){
+    alert('*White Whale Cooker Professional*\n\nProduct details:-\nProfessional Cooker 90 * 60 cm Black / Stainless\nItalian design\nTotally safe\nAll Italian burners\n Operating burners and oven with self-ignition\n Turn on the oven and grill together with the door closed\ndigital screen timer\nEquipped with a horn fan to distribute heat\nbig oven \nthe price :EGP 7,299')
+}
+function text11(){
+    alert('*Instaview Door in Door Refrigerator*\n\nProduct details:-\nenergy savings  32%\nWith LG Smart ThinQ™, you can control and diagnose your refrigerator with your smartphone.\nSpacePlus™ Ice SystDoor-mounted slim ice maker provides more storage space in the freezer compartment.  ‎')
+}
+function text12(){
+    alert('*White Point Refrigerator*\n\nProduct details:-\nModel WPR463S\nSize Feet 18\nblack color\nDepth 720 mm\nWidth 700 mm\nHeight 173 mm\nShelves are tempered glass\nthe price :9,399 EGP ')
+}
+function text13(){
+    alert('*LG Refrigerator*\n\nProduct details:-\nICE & WATER Dispeanser\nSmart Diagnosis\nEasy open Handle\nTouch LED\nInverter liner compressor\nHygiene Fresh\nMulti Air Flow\nLED Lighting\nSlim Indoor ICE Maker\nExtra space\nTempered glass shelves\nthe price:EGP 28,790')
+}
+function text14(){
+    alert('*SHARP Split Air Conditioner*\n\nProduct details:-\nSharp Air Conditioner 3HP Cool\nCooling Capacity: 23000 ( BTU/H )\nAir Conditioner Color: Silver\nInverter Technology:Advanced Technology Contribute To Reduce Costs and Energy Consumption By Switching the Compressor Between High and Low Operation Modes Instead Of Switching It On and Off\nDigital Display: Temperature Setting Display On IDU\nPlasma Cluster Ion:Sharp’s \nthe price :12,925.00 EGP')
+}
+function text15(){
+    alert('*Carrier Air Condition*\n\nProduct details:-\nCarrier Model: Optimax\nType: Split Sub Hi-Wall\nCold Only\nHorse Power	3\nthe price :EGP 11,495')
+}
+function text16(){
+    alert('*LG Air Conditioner*\n\nProduct details:-\nEnergy saving 60%\nFast Cooling Plasma  30%\nIonizer Smart ThinQ WI-FI\nYear Warranty 10\nSmart Diagnosis Energy Display Compressor\n the price:EGP 17,320 ')
+}
+
+function text18(){
+    alert('*KENWOOD KITCHEN MACHINE*\n\nProduct details:-\nBrand: Kenwood\nMixer: plastic\nRemovable bowl, Dough beater, K-beater, High speed, Low speed outlet, Whisk: Yes\nGeneral Specifications\nCase material: Die-cast metal\nBowl material: stainless steel\nBowl material: non-stick coated aluminium.\nColor: Silver\nMotor power: 1200 Watt\nSize (cm): H 34.4 x W 38.5 x D 28.0\nWatt: 1200 Watt\nWeight: 7.658 kg\nthe price : 8,700.00 EGP')
+}
+function text17(){
+    alert('*Kenwood Kitchen Machine*\n\nProduct details:-\nModel KHH324RD\nliters  4.3\nWatt 1000\nWarranty 2 years\nstainless steel bowl\norange juice yes\nNumber of accessories 6\nBlender Capacity 1.5 Liter\nthe price :EGP6,995')
+}
+
+function text19(){
+    alert('*TORNADO Kitchen Machine*\n\nProduct details:-\nTORNADO Kitchen Machine in Stainless steel Color\nElectric Power : 1200 Watt\nBowl Capacity : 5 Liter\nGlass Blender Capacity : 1.5 Liter\nCountry Of Origin : China\n1 Year Full Free Warranty\nRemovable bowl, Dough beater, K-beater, High speed, Low speed outlet, Whisk: Yes\nthe price :4,675 EGP')
+}
+
+function text20(){
+    alert('*Karcher Steam Cleaner*\n\nproduct details:-\nThe Kärcher SC 1entry-level steam cleaner with powerful cleansing action cleans hygienically without chemicals. Suitable for kitchens, bathrooms and floors.\nthe price :EGP 3,199')
+}
+function text21(){
+    alert('*Karcher Steam Cleaner*\n\nproduct details:-\nArea performance  20 m²\nHeating output 	1200 w\nMax. steam pressure 3 bar\nHeating time 	3 min\nBolier / tank 	0.2 l\nVoltage 	220 - 240  V\nFrequency 	50 - 60  Hz\n Weight without accessories 	1.58 kg\n Dimensions (L x W x H) 	320.5 x 127.4 x 185.7 mm \nthe price :EGP 2,299')
+}
+function text22(){
+    alert('*Panasonic Vacuum Cleaner*\n\nproduct details:-\nDust Capacity  2.0 L\nMax input power 1800 w\nHEPA Filter\nFull Capture Nozzle \nMetal Extension Wand x 2\nCord Length5.0 m\nthe price :EGP 2,355')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
